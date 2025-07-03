eb14b1d72d594556bbbafa8f3f782427391bbd55
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-07-04 00:29:50 +0900

    🚀 Add `ImageAsset` and `User`

---
+++ Resplash/Models/ImageAsset.swift
@@ -0,0 +1,102 @@
+//
+//  ImageAsset.swift
+//  Resplash
+//
+//  Created by 정재성 on 7/3/25.
+//
+
+import Foundation
+
+struct ImageAsset {
+  let id: String
+  let createdAt: Date
+  let updatedAt: Date
+  let type: AssetType
+  let isPremium: Bool
+
+  let description: String?
+  let likes: Int
+
+  let width: Int
+  let height: Int
+  let color: String
+  let imageURL: ImageURL
+
+  let user: User
+  let shareLink: URL
+}
+
+// MARK: - ImageAsset.AssetType
+
+extension ImageAsset {
+  enum AssetType: String, Decodable {
+    case photo
+    case illustration
+  }
+}
+
+// MARK: - ImageAsset.ImageURL
+
+extension ImageAsset {
+  struct ImageURL: Decodable {
+    let raw: URL
+    let full: URL
+    let regular: URL
+    let small: URL
+    let thumb: URL
+    let smallS3: URL
+
+    enum CodingKeys: String, CodingKey {
+      case raw
+      case full
+      case regular
+      case small
+      case thumb
+      case smallS3 = "small_s3"
+    }
+  }
+}
+
+// MARK: - ImageAsset (Identifiable)
+
+extension ImageAsset: Identifiable {
+}
+
+// MARK: - ImageAsset (Hashable)
+
+extension ImageAsset: Hashable {
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.id == rhs.id && lhs.updatedAt == rhs.updatedAt
+  }
+}
+
+// MARK: - ImageAsset (Decodable)
+
+extension ImageAsset: Decodable {
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: StringCodingKey.self)
+    self.id = try container.decode(String.self, forKey: "id")
+    self.createdAt = try container.decode(Date.self, forKey: "created_at")
+    self.updatedAt = try container.decode(Date.self, forKey: "updated_at")
+    self.type = try container.decode(AssetType.self, forKey: "asset_type")
+    self.isPremium = try container.decode(Bool.self, forKey: "premium")
+
+    let description = try container.decodeIfPresent(String.self, forKey: "description")
+    let altDescription = try container.decodeIfPresent(String.self, forKey: "alt_description")
+    self.description = description ?? altDescription
+    self.likes = try container.decode(Int.self, forKey: "likes")
+
+    self.width = try container.decode(Int.self, forKey: "width")
+    self.height = try container.decode(Int.self, forKey: "height")
+    self.color = try container.decode(String.self, forKey: "color")
+    self.imageURL = try container.decode(ImageURL.self, forKey: "urls")
+
+    self.user = try container.decode(User.self, forKey: "user")
+    let linkContainer = try container.nestedContainer(keyedBy: StringCodingKey.self, forKey: "links")
+    self.shareLink = try linkContainer.decode(URL.self, forKey: "html")
+  }
+}
---
+++ Resplash/Models/User.swift
@@ -0,0 +1,102 @@
+//
+//  User.swift
+//  Resplash
+//
+//  Created by 정재성 on 7/4/25.
+//
+
+import Foundation
+
+struct User {
+  let id: String
+  let userId: String
+  let updatedAt: Date
+
+  let name: String
+  let bio: String?
+  let location: String?
+  let imageURL: ImageURL
+
+  let totalLikes: Int
+  let totalCollections: Int
+  let totalPhotos: Int
+  let totalIllustrations: Int
+
+  let socials: [Social]
+  let shareLink: URL
+}
+
+// MARK: - User.ImageURL
+
+extension User {
+  struct ImageURL: Decodable {
+    let small: URL
+    let medium: URL
+    let large: URL
+  }
+}
+
+// MARK: - User.Social
+
+extension User {
+  enum Social {
+    case twitter(String)
+    case instagram(String)
+    case portfolio(URL)
+  }
+}
+
+// MARK: - User (Identifiable)
+
+extension User: Identifiable {
+}
+
+// MARK: - User (Hashable)
+
+extension User: Hashable {
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.id == rhs.id && lhs.updatedAt == rhs.updatedAt
+  }
+}
+
+// MARK: - User (Decodable)
+
+extension User: Decodable {
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: StringCodingKey.self)
+    self.id = try container.decode(String.self, forKey: "id")
+    self.userId = try container.decode(String.self, forKey: "username")
+    self.updatedAt = try container.decode(Date.self, forKey: "updated_at")
+
+    self.name = try container.decode(String.self, forKey: "name")
+    self.bio = try container.decodeIfPresent(String.self, forKey: "bio")
+    self.location = try container.decodeIfPresent(String.self, forKey: "location")
+    self.imageURL = try container.decode(ImageURL.self, forKey: "profile_image")
+
+    self.totalLikes = try container.decode(Int.self, forKey: "total_likes")
+    self.totalCollections = try container.decode(Int.self, forKey: "total_collections")
+    self.totalPhotos = try container.decode(Int.self, forKey: "total_photos")
+    self.totalIllustrations = try container.decode(Int.self, forKey: "total_illustrations")
+
+    let socialContainer = try container.nestedContainer(keyedBy: StringCodingKey.self, forKey: "social")
+    self.socials = try socialContainer.allKeys.compactMap {
+      switch $0.stringValue {
+      case "twitter_username":
+        return try socialContainer.decodeIfPresent(String.self, forKey: $0).map { .twitter($0) }
+      case "instagram_username":
+        return try socialContainer.decodeIfPresent(String.self, forKey: $0).map { .instagram($0) }
+      case "portfolio_url":
+        return try socialContainer.decodeIfPresent(URL.self, forKey: $0).map { .portfolio($0) }
+      default:
+        return nil
+      }
+    }
+
+    let linkContainer = try container.nestedContainer(keyedBy: StringCodingKey.self, forKey: "links")
+    self.shareLink = try linkContainer.decode(URL.self, forKey: "html")
+  }
+}
