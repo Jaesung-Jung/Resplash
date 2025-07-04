57af5deece52cbc2aa09cae2178c7ac52c055efd
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-07-04 17:31:56 +0900

    🚀 Add view hierarchy

---
+++ Resplash/ViewControllers/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  Resplash
+//
+//  Created by 정재성 on 7/4/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+}
