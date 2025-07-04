6090b244ca0b297f9317e3f7fac25a165394ac04
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-07-04 11:45:13 +0900

    🚀 Add page representation

---
+++ Resplash/Models/Page.swift
@@ -0,0 +1,31 @@
+//
+//  Page.swift
+//  Resplash
+//
+//  Created by 정재성 on 7/4/25.
+//
+
+struct Page<Base: Collection> {
+  typealias Element = Base.Element
+  typealias Index = Base.Index
+  typealias Iterator = Base.Iterator
+
+  let number: Int
+  let items: Base
+}
+
+// MARK: - Page (Collection)
+
+extension Page: Collection {
+  @inlinable var startIndex: Index { items.startIndex }
+  @inlinable var endIndex: Index { items.endIndex }
+
+  @inlinable func index(after i: Index) -> Index { items.index(after: i) }
+  @inlinable subscript(position: Index) -> Element { items[position] }
+
+  @inlinable var count: Int { items.count }
+
+  @inlinable func makeIterator() -> Iterator {
+    items.makeIterator()
+  }
+}
