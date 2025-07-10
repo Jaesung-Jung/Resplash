929cfd10d5368a81fc31a078ea50de24f5354b27
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-07-10 16:08:56 +0900

    🛠️ Fix main actor isolated error

--- Resplash/Extensions/RxCocoa+Extension.swift
+++ Resplash/Extensions/RxCocoa+Extension.swift
@@ -14,14 +14,16 @@ import RxCocoa
 extension Reactive where Base: UIScrollView {
   func reachedBottom(offset: CGFloat = 0.0) -> ControlEvent<Void> {
     let source = contentOffset
-      .map { contentOffset in
-        let visibleHeight = self.base.frame.height - self.base.contentInset.top - self.base.contentInset.bottom
-        let y = contentOffset.y + self.base.contentInset.top
-        let threshold = max(offset, self.base.contentSize.height - visibleHeight)
-        return y >= threshold
+      .asObservable()
+      .withUnretained(base)
+      .filter { base, contentOffset in
+        MainActor.assumeIsolated {
+          let visibleHeight = base.frame.height - base.contentInset.top - base.contentInset.bottom
+          let y = contentOffset.y + base.contentInset.top
+          let threshold = max(offset, base.contentSize.height - visibleHeight)
+          return y >= threshold
+        }
       }
-      .distinctUntilChanged()
-      .filter { $0 }
       .map { _ in }
     return ControlEvent(events: source)
   }
