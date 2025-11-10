bdc12e80fc7dc3deae01efad784c900b4c69a8c1
Jaesung Jung <jaesung.jung1986@gmail.com>
2025-11-10 13:38:03 +0900

    🛠️ Fix preview image size

--- Resplash/Sources/Models/ImageAsset.swift
+++ Resplash/Sources/Models/ImageAsset.swift
@@ -97,10 +97,31 @@ extension ImageAsset {
     isPremium: false,
     description: "Close-up of the SumUp Solo card reader, held by a store owner in a boutique setting. Designed for small businesses that want to accept contactless card and mobile payments with style and ease.",
     likes: 4920,
-    width: 5196,
-    height: 3464,
+    width: 9972,
+    height: 9972,
     color: "#a6a6a6",
     url: .preview,
+    user: .preview,
+    shareLink: URL(string: "https://unsplash.com/photos/DIdhesLS07I")!
+  )
+
+  static let preview2 = ImageAsset(
+    id: "y3AuJts5x1Y",
+    slug: "화이트-포르쉐-911의-리어-엔드가-보인다-y3AuJts5x1Y",
+    createdAt: .now,
+    updatedAt: .now,
+    type: .photo,
+    isPremium: false,
+    description: "Porsche 911T",
+    likes: 4920,
+    width: 6336,
+    height: 9504,
+    color: "#f3f3f3",
+    url: ImageURL(
+      raw: URL(string: "https://images.unsplash.com/photo-1750210955902-ce0e71765fb1?ixid=M3wxMjA3fDB8MXxhbGx8MXx8fHx8fHx8MTc1Njk0NTgxOXw&ixlib=rb-4.1.0")!,
+      full: URL(string: "https://images.unsplash.com/photo-1750210955902-ce0e71765fb1?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMjA3fDB8MXxhbGx8MXx8fHx8fHx8MTc1Njk0NTgxOXw&ixlib=rb-4.1.0&q=85")!,
+      s3: URL(string: "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1750210955902-ce0e71765fb1")!
+    ),
     user: .preview,
     shareLink: URL(string: "https://unsplash.com/photos/DIdhesLS07I")!
   )
